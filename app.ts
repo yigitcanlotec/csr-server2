739ffeaa195724d7cf959a669888c7cf158e2940
YigitJohn <yigitcanustek@gmail.com>
2023-10-18 01:11:46 +0300

    Fixed authorization and crud bugs.

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import bcrypt = require("bcrypt");
 const app = express();
 const port = 3000;
 
-app.use(cors({credentials: true}));
+app.use(cors({credentials: true,  origin: ['http://localhost:5500', 'http://127.0.0.1:5500']}));
 app.use(express.json());
 
 function generateRandomString(length: number) {
@@ -42,7 +42,7 @@ const pool = new Pool({
 
 
 const isAuthenticated = (req, res, next) =>{
-  console.log('auth req');
+ 
   if (req.headers.authorization){
     const userToken = req.headers.authorization.split(' ')[1];
     const queryString = `SELECT * FROM userdb WHERE (session_key = $1);`;
@@ -53,75 +53,15 @@ const isAuthenticated = (req, res, next) =>{
         res.sendStatus(401);
       }
     })
-    // const userIdentity = atob(req.headers.authorization.split(' ')[1]).split(':');
-    // if (req.body.token){
-    //   const queryString = `SELECT session_key FROM userdb WHERE (username = $1);`;
-      
-      
-    //   pool.query(queryString, [userIdentity[0]]).then((result) => {
-    //       if (result.rowCount === 1) {
-    //           if (result.rows[0].session_key === req.body.token) {
-    //             next();
-    //           } else {
-    //             res.sendStatus(401);
-    //           }
-    //       } else {
-    //         res.sendStatus(401);
-    //       }
-    //   }).catch((error) => res.status(400).send(error))
-    // } else {
-    //   res.sendStatus(400);
-    // }
   } else {
     res.sendStatus(400);
   }
   
 }
 
-app.options('/api/v1/login', cors({credentials: true, origin: 'http://localhost:5500',}), (req, res) => {
-  res.sendStatus(200);
-})
-
-// app.get('/api/v1/login', cors({credentials: true, origin: 'http://localhost:5500',}), (req, res) => {
-
-    
-//     let queryString = "SELECT * FROM userdb WHERE (username = $1);";
-//     const userIdentity = atob(req.headers.authorization.split(' ')[1]).split(':');
-//     pool.query(queryString, [userIdentity[0]]).then((queryData) => {
-//      console.log(queryData);
-//       if (queryData.rowCount === 1){
-   
-//         if (queryData.rows[0].username === userIdentity[0]){
-        
-//           bcrypt.compare(userIdentity[1], queryData.rows[0].password).then((isAuth) => {
-//             if (isAuth) {
-//                 console.log('5');
-//                 const randStr = generateRandomString(16);
-//                 queryString = `UPDATE userdb SET session_key=$1 WHERE (username = $2);`;
-//                 pool.query(queryString, [randStr, userIdentity[0]]);
-//                 res.status(200).send(randStr);
-//               } else {
-//                return res.sendStatus(401);
-                
-//               }
-//           }).catch((err) => {
-//             console.log('4');
-//             return res.sendStatus(400);
-//           });
 
 
-//           return;
-//         } else {
-//           return res.sendStatus(401);
-//         }
-//       }
-//     }).catch((err) => {return res.status(418).send(err)})
-//      res.sendStatus(401);
- 
- 
-// });
-
-app.get('/api/v1/login', cors({credentials: true, origin: 'http://localhost:5500',}), async (req, res) => {
+app.get('/api/v1/login', async (req, res) => {
   let queryString = "SELECT * FROM userdb WHERE (username = $1);";
   const userCredentials = atob(req.headers.authorization.split(' ')[1]).split(':');
   const isUserExists = (await pool.query(queryString, [userCredentials[0]]))
@@ -140,6 +80,7 @@ app.get('/api/v1/login', cors({credentials: true, origin: 'http://localhost:5500
     res.sendStatus(401);
 });
 
+
 app.post('/api/v1/register', async (req, res, next) => {
   const queryString = 'INSERT INTO userdb(username, password) VALUES($1,$2);'
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -152,21 +93,20 @@ app.post('/api/v1/register', async (req, res, next) => {
   
 });
 
-
 app.get('/api/v1/tasks/', isAuthenticated, async (req, res) => {
     let queryString: string;
     let queryData: Array<QueryResult>;
-    console.log(req.query, req.headers.authorization);
+    // console.log(req.query, req.headers.authorization);
     if (req.query.filter === undefined || req.query.filter === 'all') {
         if (req.headers.authorization){
           // const userIdentity = atob(req.headers.authorization.split(' ')[1]).split(':');
           const userToken = req.headers.authorization.split(' ')[1];
           queryString =  `SELECT username FROM userdb WHERE (session_key = $1);`
           const userName =  (await pool.query(queryString, [userToken])).rows[0].username;
-          console.log(userName);
+          // console.log(userName);
           queryString = 'SELECT * FROM todo WHERE (assignee = $1) ORDER BY id ASC';
           queryData = (await pool.query(queryString, [userName])).rows;
-          return res.status(200).send(queryData);
+          return res.status(200).send({queryData: queryData, username: userName});
         } else {
           return res.sendStatus(401);
         }
@@ -176,15 +116,16 @@ app.get('/api/v1/tasks/', isAuthenticated, async (req, res) => {
         const userToken = req.headers.authorization.split(' ')[1];
         queryString =  `SELECT username FROM userdb WHERE (session_key = $1);`
         const userName =  (await pool.query(queryString, [userToken])).rows[0].username;
-        console.log(userName);
+        // console.log(userName);
         queryString = `SELECT * FROM todo WHERE (assignee = $1 AND done=$2) ORDER BY id ASC`;
         queryData = (await pool.query(queryString, [userName, req.query.filter])).rows;
-        return res.status(200).send(queryData);
+        return res.status(200).send({queryData: queryData, username: userName});
       } else {
         return res.sendStatus(401);
       }
     }
 });
+
 
 
 app.get('/api/v1/:user/:userId', isAuthenticated ,async (req, res) => {
@@ -200,6 +141,7 @@ app.post('/api/v1/tasks/insert', isAuthenticated, async (req, res) => {
   const userToken = req.headers.authorization.split(' ')[1];
   queryString =  `SELECT username FROM userdb WHERE (session_key = $1);`
   const userName =  (await pool.query(queryString, [userToken])).rows[0].username;
+  // console.log(userName);
   queryString = `INSERT INTO todo (title, assignee, done) values ($1,$2,$3);`;
   await pool.query(queryString, [ formData.title, userName, formData.done ]);
 
@@ -208,12 +150,12 @@ app.post('/api/v1/tasks/insert', isAuthenticated, async (req, res) => {
 });
 
 
-app.delete('/api/v1/task/:taskId/delete', isAuthenticated,  async (req, res) => {
+app.post('/api/v1/task/:taskId/delete', isAuthenticated,  async (req, res) => {
   const formData = req.body;
-  console.log(formData);
+  // console.log(formData);
 
       const queryString = 'DELETE FROM todo WHERE (id = $1 AND assignee = $2);';
-      await pool.query(queryString, [req.params.taskId, req.body.username]);
+      await pool.query(queryString, [req.params.taskId , req.body.assignee]);
 
       res.sendStatus(200);
   
@@ -224,7 +166,7 @@ app.post('/api/v1/task/:taskId/done', isAuthenticated, async (req, res) => {
                           SET
                               done=true 
                           WHERE (id = $1 AND assignee = $2);`;
-  await pool.query(queryString, [req.params.taskId, req.body.username]);
+  await pool.query(queryString, [req.params.taskId, req.body.assignee]);
   res.sendStatus(200);
 });
 
@@ -233,19 +175,20 @@ app.post('/api/v1/task/:taskId/undone', async (req, res) => {
                             SET
                                 done=false 
                             WHERE (id = $1 AND assignee = $2);`;
-  await pool.query(queryString, [req.params.taskId, req.body.username]);
+  await pool.query(queryString, [req.params.taskId, req.body.assignee]);
   res.sendStatus(200);
 });
 
 
 app.post('/api/v1/task/:taskId/delete', async (req, res) => {
   const queryString = `DELETE FROM todo WHERE (id = $1 AND assignee = $2);`;
-  await pool.query(queryString, [req.params.taskId, req.body.username]);
+  await pool.query(queryString, [req.params.taskId, req.body.assignee]);
   res.sendStatus(200);
 });
 
 
-app.post('/api/v1/task/:taskId/update',async (req, res) => {
+app.post('/api/v1/task/:taskId/update', (req, res) => {
+    // console.log(req.body);
     const queryString = `UPDATE todo 
     SET
         title=$1,
@@ -254,14 +197,24 @@ app.post('/api/v1/task/:taskId/update',async (req, res) => {
 
 
   pool
-  .query(queryString, [req.body.title, req.body.done, req.params.taskId, req.body.username ])
-  .then(() => {
+  .query(queryString, [req.body.title, req.body.done, req.body.id, req.body.assignee ])
+  .then((queryResult) => {
+    // console.log(queryResult);
     res.sendStatus(200);
   })
   .catch((err) => {
   res.send(err);
   });
 })
+
+app.get('/api/v1/userInfo', isAuthenticated, async (req, res) =>{
+  const queryString =  `SELECT (username, password) FROM userdb WHERE (session_key = $1);`;
+  const userToken = req.headers.authorization.split(' ')[1];
+  const queryData = (await pool.query(queryString, [userToken])).rows[0];
+  const credentials = queryData.row.substring(1, queryData.row.length-1).split(',');
+
+    res.status(200).send(credentials);
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
