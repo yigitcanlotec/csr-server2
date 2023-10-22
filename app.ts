fbe612b71e36faf3f51d71fdca05800a06baa25e
Yiğitcan Üstek <146730128+yigitcanlotec@users.noreply.github.com>
2023-10-23 01:14:19 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 app.use(
     cors({
         credentials: true,
-        origin: '51.20.126.62',
+        origin: '51.20.126.62:80',
     })
 );
 app.use(express.json());
