bcb33a23d13b2774a39315dd3483ebbe6aa8f053
YigitJohn <yigitcanustek@gmail.com>
2023-10-22 19:02:15 +0300

    Changed default DB SSL config.

--- app.ts
+++ app.ts
@@ -67,6 +67,9 @@ const pool = new Pool({
     database: process.env.DB_NAME || 'todo_app',
     password: process.env.DB_PASSWORD || 'pass123',
     port: parseInt(process.env.DB_PORT) || 5432,
+    ssl: {
+      rejectUnauthorized:false
+    }
 });
 
 // Middleware for user authorization.
